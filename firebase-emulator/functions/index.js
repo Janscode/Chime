8ea32e37f12f21f4ae8f4b82c760033b5addd49a
Janscode <55261974+Janscode@users.noreply.github.com>
2021-02-04 19:11:47 -0700

    Modified addQuestionToCampaign to add question to recipient queues

--- firebase-emulator/functions/index.js
+++ firebase-emulator/functions/index.js
@@ -11,6 +11,7 @@ const db = admin.firestore();
 //   response.send("Hello from Firebase!");
 // });
 
+//todo: make this work for local emulator
 exports.createUser = functions.auth.user().onCreate((user) => {
     const { uid, displayName, email } = user;
 
@@ -24,11 +25,23 @@ exports.addQuestionToCampaign = functions.firestore.document('questions/{questio
     .onCreate((snap, context) => {
         const questionId = context.params.questionId;
         const campaignId = snap.data().campaignId;
+        const userRef = db.collection("users");
 
         const campaignRef = db.collection('campaigns');
         campaignRef.doc(campaignId).get()
             .then((doc) => {
                 if (doc.exists) {
+                    console.log(doc.data().recipients);
+                    //Write question to user feeds
+                    doc.data().recipients.forEach(uid => {
+                        console.log(uid);
+                        userRef.doc(uid).get().then((doc) => {
+                            //TODO: am I doing this async right?
+                            doc.ref.update({
+                                queue: admin.firestore.FieldValue.arrayUnion(questionId),
+                            });
+                        });
+                    });
                     // Write the question to the campaign list
                     return doc.ref.update({
                         questions: admin.firestore.FieldValue.arrayUnion(questionId),
